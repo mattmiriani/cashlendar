e23a5ddead3600d14286748503bb80864175875d
matt <matheusmiriani@hotmail.com>
2025-01-22 00:08:06 -0300

    Initial commit

---
+++ server/src/main/java/com/matt/cashlendar/CashlendarApplication.java
@@ -0,0 +1,13 @@
+package com.matt.cashlendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashlendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashlendarApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/matt/cashlendar/CashlendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matt.cashlendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashlendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
